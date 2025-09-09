9e80b6bc4b35dd57923db8e8404f05a481bafe4b
Faseena <faseenakv@gmail.com>
2025-09-09 20:51:14 +0530

    Fixed highlighted text

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -4,9 +4,8 @@ package ex_01_Java_Basics;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
