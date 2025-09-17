ad99c231a90ac1c0cabe4c68d81c2f7f48e64518
Faseena <faseenakv@gmail.com>
2025-09-17 12:46:20 +0530

    Labs for Literal, Operators, Variables

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -5,7 +5,7 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
---
+++ src/ex_02_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,41 @@
+package ex_02_Java_Basics;
+/**
+ * Author : Faseena Kv
+ * Course :  ATB14x
+ * Learning :  Java
+ *
+ **/
+
+public class Lab006_Comments_Multi {
+    /**
+     * Author : Faseena Kv
+     * Course :  ATB14x
+     * Learning :  Java
+     *
+     **/
+
+    // This is sample
+    public static void main(String[] args) {
+        /**
+         * Author : Faseena Kv
+         * Course :  ATB14x
+         * Learning :  Java
+         *
+         **/
+
+       /*
+
+        System.out.println("Heloo");
+        System.out.println("Heloo");
+
+        */
+
+
+    }
+}
+/**
+ * Author : Faseena Kv
+ * Course :  ATB14x
+ * Learning :  Java
+ *
+ **/
